34d43323ba551d5c7495729ea0382f4ce8f88581
VOZ <voisfce@gmail.com>
2023-04-04 21:59:17 +0200

    Solutions Trier et inverseV

--- PAP2223/src/vect/MyVect.java
+++ PAP2223/src/vect/MyVect.java
@@ -61,10 +61,32 @@ public class MyVect {
 	 * @param v
 	 * @return
 	 */
-	public static boolean estTrie(int[] v) {
+	public static boolean estTrieV1(int[] v) {
+		assert v.length>0:"Le vecteur ne peut pas être vide";
+		int i = 0;
+		int l = v.length - 1;
+		while (i < l && v[i] <= v[i + 1])
+			i++;
 
-		// TODO
-		return true; // A changer
+		return i == l;
+	}
+
+	/**
+	 * Vérifie si un vecteur est trie
+	 * 
+	 * @param v
+	 * @return
+	 */
+	public static boolean estTrieV2(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int i = 0;
+		int l = v.length - 1;
+		boolean trie = true;
+		while (i < l && trie) {
+			trie = v[i] <= v[i + 1];
+			i++;
+		}
+		return trie;
 	}
 
 	/**
@@ -72,14 +94,41 @@ public class MyVect {
 	 * 
 	 * @param v le vecteur qui sera modifié
 	 */
-	public static void inverseV(int[] v) {
+	public static void inverseV1(int[] v) {
+		int fin = v.length / 2;
+		int l = v.length - 1;
+		int tmp;
+		for (int i = 0; i < fin; i++) {
+			tmp = v[i];
+			v[i] = v[l - i];
+			v[l - i] = tmp;
+		}
+	}
 
+	/**
+	 * Inverse le contenu d'un vecteur
+	 * 
+	 * @param v le vecteur qui sera modifié
+	 */
+	public static void inverseV2(int[] v) {
+		int i = 0;
+		int j = v.length - 1;
+		int tmp;
+		while (i < j) {
+			tmp = v[i];
+			v[i] = v[j];
+			v[j] = tmp;
+			i++;
+			j--;
+		}
 	}
 
 	public static void main(String[] args) {
-		int[] v1 = {};
-		int[] v2 = { 2, 8, 11, 67, 4 };
-		afficheV(v1);
+		// int[] v1 = {};
+		int[] v2 = { 2, 8, 11, 67, 94 };
+		// afficheV(v1);
+		afficheV(v2);
+		System.out.println("Trié: " + estTrieV2(v2));
 		afficheV(v2);
 
 	}
--- PAP2223/test/vect/TestMyVect.java
+++ PAP2223/test/vect/TestMyVect.java
@@ -9,6 +9,9 @@ class TestMyVect {
 	static int[] v0 = {};
 	static int[] v1 = { 5 };
 	static int[] v2 = { 1, 2, 6, 9 };
+	static int[] v3 = { 1, 2, 6, 4 };
+	static int[] v4 = { 1, 0, 6, 9 };
+	static int[] v5 = { 1, 1 };
 
 	@Test
 	void testSommeV() {
@@ -33,4 +36,61 @@ class TestMyVect {
 		assertArrayEquals(v2, copie);
 	}
 
+	@Test
+	void testTrieV1() {
+		assertTrue(MyVect.estTrieV1(v1));
+		assertTrue(MyVect.estTrieV1(v2));
+		assertTrue(MyVect.estTrieV1(v5));
+		assertFalse(MyVect.estTrieV1(v3));
+		assertFalse(MyVect.estTrieV1(v4));
+	}
+
+	@Test
+	void testTrieV2() {
+		assertTrue(MyVect.estTrieV2(v1));
+		assertTrue(MyVect.estTrieV2(v2));
+		assertTrue(MyVect.estTrieV2(v5));
+		assertFalse(MyVect.estTrieV2(v3));
+		assertFalse(MyVect.estTrieV2(v4));
+	}
+
+	@Test
+	void testInverseV1() {
+		int[] v0 = {};
+		int[] vi0 = {};
+		int[] v1 = { 2 };
+		int[] vi1 = { 2 };
+		int[] v2 = { 1, 2, 3 };
+		int[] vi2 = { 3, 2, 1 };
+		int[] v3 = { 1, 2, 3, 4 };
+		int[] vi3 = { 4, 3, 2, 1 };
+		MyVect.inverseV1(v0);
+		assertArrayEquals(vi0, v0);
+		MyVect.inverseV1(v1);
+		assertArrayEquals(vi1, v1);
+		MyVect.inverseV1(v2);
+		assertArrayEquals(vi2, v2);
+		MyVect.inverseV1(v3);
+		assertArrayEquals(vi3, v3);
+	}
+
+	@Test
+	void testInverseV2() {
+		int[] v0 = {};
+		int[] vi0 = {};
+		int[] v1 = { 2 };
+		int[] vi1 = { 2 };
+		int[] v2 = { 1, 2, 3 };
+		int[] vi2 = { 3, 2, 1 };
+		int[] v3 = { 1, 2, 3, 4 };
+		int[] vi3 = { 4, 3, 2, 1 };
+		MyVect.inverseV2(v0);
+		assertArrayEquals(vi0, v0);
+		MyVect.inverseV2(v1);
+		assertArrayEquals(vi1, v1);
+		MyVect.inverseV2(v2);
+		assertArrayEquals(vi2, v2);
+		MyVect.inverseV2(v3);
+		assertArrayEquals(vi3, v3);
+	}
 }
